04af33fdeb660a730a1b82e357e04ed3757d5fdc
michaeljaren <michaelbarragan.j.g@gmail.com>
2025-10-15 09:04:37 -0500

    corrrecion de la prueba
    correccion de la prueba 1 de POO

--- e04Prueba1.java
+++ e04Prueba1.java
@@ -1,49 +1,47 @@
 /* UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE
   SOFTWARE 
-NOMBRE: MICHAEL BARRAGAN        PERIODO: PREGRADO
-Asignatura:                     NRC:29762
-Fecha: 8/10/2025
-Tema: Prueba 1 del cuadro de caracteres en java
+  NOMBRE: MICHAEL BARRAGAN        PERIODO: PREGRADO
+  Asignatura: POO                     NRC:29762
+  Fecha: 8/10/2025
+  Tema: Prueba 1 - Cuadro de caracteres alternado
 */ 
 
-//importamos la libreria scanner
 import java.util.Scanner;
 
 public class e04Prueba1 {
-    public static void main(String [] args ){
-  //Creamos el objeto Scanner para leer datos(Instanciar = crear e inicializar)
-    Scanner leer = new Scanner (System.in);  
-    //Declaramos variables
-    int numero=0;
-    char caracter;
-        //Ingresamos Datos
-        System.out.print("Ingrese un numero: " );
-        numero=leer.nextInt();
-        System.out.print("Ingrese un caracter: " );
-        caracter=leer.next().charAt(0);
-        //proceso
-        for(int i = 0; i<numero;i++){
-            for(int j=0; j<numero;j++){
-                System.out.print(caracter);
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int numero;
+        char caracter;
+
+        System.out.print("Ingrese un numero:  ");
+        numero = leer.nextInt();
+        System.out.print("Ingrese un caracter:  ");
+        caracter = leer.next().charAt(0);
+
+        // Proceso para formar el patrón
+        for (int i = 0; i < numero; i++) {
+            if (i % 2 == 0) { // líneas llenas
+        for (int j = 0; j < numero; j++) {
+            System.out.print(caracter);
             }
-            System.out.print(" \n");
-        
+        } else { // líneas huecas
+        for (int j = 0; j < numero; j++) {
+            if (j == 0 || j == numero - 1) {
+                System.out.print(caracter);
+            } else {
+                System.out.print(" ");
+            }
         }
+    }
+        System.out.println();
+    }
         //Mostramos datos
         System.out.println("El caracter es: " +caracter);
         System.out.println("El numero es: "+numero);
         
-        
-        
-        
-        
-    
-    
-    
-    
     }
-    
-    
-    
-    
 }
+
+        
+       
